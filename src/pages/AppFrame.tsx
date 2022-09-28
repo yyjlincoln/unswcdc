1d24972dec6035a27d37c09171b3fbde5c18c07c
Lincoln Yan <yyjlincoln@gmail.com>
2022-09-28 11:09:17 +1000

    cleanup

--- src/pages/AppFrame.tsx
+++ src/pages/AppFrame.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export const AppFrame: React.FC = () => {
   return (
     <div
-      className="flex flex-col overflow-auto"
+      className="flex flex-col"
       style={{
         height: "100vh",
         width: "100vw",
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -26,7 +26,7 @@ export const Main: React.FC = () => {
             }}
             id="background_animate"
           >
-            <div className="w-screen h-screen bg-black left-0 top-0 opacity-30 absolute overflow-hidden"></div>
+            <div className="w-screen h-screen left-0 top-0 opacity-30 absolute overflow-hidden bg-gradient"></div>
             <img
               className="object-cover w-screen h-screen overflow-hidden"
               src="https://images.unsplash.com/photo-1544531586-fde5298cdd40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
@@ -60,6 +60,7 @@ export const Main: React.FC = () => {
           </div>
         </div>
       </div>
+      <div className="flex flex-col justify-center text-center py-4 px-4 heading-font font-light">新南华辩社</div>
     </>
   );
 };
