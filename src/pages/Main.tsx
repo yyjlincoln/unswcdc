05aacd8e183a483f54c834c413a22a42b2700ce7
Lincoln Yan <yyjlincoln@gmail.com>
2022-09-28 12:11:02 +1000

    [feat] Added background marquee effect

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -19,15 +19,31 @@ export const Main: React.FC = () => {
 
   const getMarqueeElements = () => {
     const el: JSX.Element[] = [];
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 10; i++) {
       el.push(
         <div
           key={i}
-          className="text-8xl text-white px-4"
-          style={{ width: "fit-content" }}
+          className="text-9xl text-white px-4"
         >
-          新南华辩社
+          {i % 2 === 0 ? "新南华辩社" : "不止辩论"}
         </div>
+      );
+    }
+    return <>{el}</>;
+  };
+
+  const getMarquees = () => {
+    const el: JSX.Element[] = [];
+    for (let i = 0; i < 30; i++) {
+      el.push(
+        <Marquee
+          gradient={false}
+          className="heading-font text-white text-4xl flex-shrink-0"
+          direction={i % 2 === 0 ? "right" : "left"}
+          delay={-i * 5}
+        >
+          {getMarqueeElements()}
+        </Marquee>
       );
     }
     return <>{el}</>;
@@ -44,19 +60,45 @@ export const Main: React.FC = () => {
             }}
             id="background_animate"
           >
-            <div className="w-screen h-screen left-0 top-0 opacity-30 absolute overflow-hidden bg-gradient"></div>
+            <div
+              className="w-screen h-screen left-0 top-0 opacity-50 absolute overflow-hidden bg-black"
+              style={{
+                zIndex: 3,
+              }}
+            ></div>
+            <div
+              className="absolute flex flex-col -rotate-45 origin-left space-y-4 opacity-10"
+              style={{
+                width: "max(200vw, 200vh)",
+                height: "max(200vw, 200vh)",
+                left: "-calc(0.5 * max(200vw, 200vh))",
+                top: "-calc(0.5 * max(200vw, 200vh))",
+                zIndex: 2,
+              }}
+            >
+              {getMarquees()}
+            </div>
             <img
               className="object-cover w-screen h-screen overflow-hidden"
               src="https://images.unsplash.com/photo-1544531586-fde5298cdd40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
               alt="background"
+              style={{
+                zIndex: 1,
+              }}
             />
+            <div
+              className="w-screen h-screen bg-black"
+              style={{
+                zIndex: 1,
+              }}
+            ></div>
           </div>
           <div
             id="title_animate"
             className="relative mx-auto text-center align-middle py-10 space-y-4"
           >
             <CenteredLogo />
-            <div className="overflow-hidden">
+            <div className="overflow-hidden drop-shadow-lg">
               <span className="relative text-6xl md:text-7xl lg:text-8xl block text-white overflow-hidden py-1 heading-font">
                 我们 不止辩论
               </span>
@@ -77,11 +119,6 @@ export const Main: React.FC = () => {
             </AnimatedButton>
           </div>
         </div>
-      </div>
-      <div className="absolute bottom-10 w-full select-none">
-        <Marquee gradient={false} className="heading-font text-white text-4xl">
-          {getMarqueeElements()}
-        </Marquee>
       </div>
     </>
   );
